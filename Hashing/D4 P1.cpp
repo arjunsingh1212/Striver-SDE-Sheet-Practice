66c33986bc89858882946625428cc8f1ba858bf2
Arjun Singh <arjun.singh981212@gmail.com>
2021-01-12 18:38:59 +0530

    Day 4 Started

---
+++ Hashing/D4 P1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.count(target-nums[i]))
+            {
+                ans={i,mp[target-nums[i]]};
+                break;
+            }
+            else
+            {
+                mp[nums[i]]=i;
+            }
+        }
+        return ans;
+    }
+};
